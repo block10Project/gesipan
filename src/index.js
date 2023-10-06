42937c4e6dee440dfd9a0be0560b12e238596d4a
subin <github@subin.kr>
2023-10-06 09:58:07 +0900

    feat: route 작성

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const boardController = require("./controller/board.controller");
 router.get("/", (req, res) => {
   res.render("index.html");
 });
+router.get("/search", (req, res) => {
+  res.render("search.html");
+});
 
 router.use("/users", userController);
 router.use("/boards", boardController);
---
+++ src/route/board.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const boardController = require("../controller/board.controller");
+const router = express.Router();
+
+router.get("/write", boardController.getWrite);
+router.post("/write", boardController.postWrite);
+
+router.get("/read", boardController.getRead);
+
+router.get("/good", boardController.getGood);
+
+router.get("/following", boardController.getFollowing);
+
+router.get("/unfollowing", boardController.getUnfollowing);
+
+router.get("/modify", boardController.getModify);
+router.post("/modify", boardController.postModify);
+
+router.get("/delete", boardController.getDelete);
+
+router.post("/comment", boardController.postComment);
