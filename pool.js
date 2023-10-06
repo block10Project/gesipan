e88c72f723f12696d1013f81987c5cd87fd3084a
subin <github@subin.kr>
2023-10-06 17:48:45 +0900

    fix: dotenv 적용

--- pool.js
+++ pool.js
@@ -1,10 +1,11 @@
 const mysql2 = require("mysql2/promise");
+require("dotenv").config();
 const pool = mysql2.createPool({
-  host: "127.0.0.1",
-  port: "3306",
-  user: "root",
-  password: "p@ssw0rd",
-  database: "login",
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 module.exports = pool;
