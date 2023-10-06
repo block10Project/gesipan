afe1f513b804e7956ea7e429c2b9cbf966d22051
subin <github@subin.kr>
2023-10-06 11:55:36 +0900

    feat: main.controller nunjucks 작성

--- src/controller/main.controller.js
+++ src/controller/main.controller.js
@@ -1,12 +1,20 @@
 const mainService = require("../service/main.service");
 
 exports.getList = (req, res) => {
-  res.render("index.html");
+  res.render("index.html", {
+    user: {
+      uid: mainService.selectUserUid(),
+    },
+    boards: mainService.selectBoards(),
+  });
 };
 
 exports.postSearch = (req, res) => {
-  res.redirect("search.html");
+  res.redirect(`search.html?keyword=${req.body.keyword}`);
 };
 exports.getSearch = (req, res) => {
-  res.render("search.html");
+  res.render("search.html", {
+    keyword: req.query.keyword,
+    boards: mainService.selectBoards(keyword),
+  });
 };
