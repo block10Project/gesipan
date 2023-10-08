522045a0acda19639256bbceb89dc61f3097f5c8
subin <github@subin.kr>
2023-10-08 18:48:47 +0900

    fix: pagination 적용

--- src/repository/main.repository.js
+++ src/repository/main.repository.js
@@ -14,12 +14,31 @@ exports.selectUserUid = async (id) => {
   }
 };
 
-exports.selectBoards = async () => {
+exports.selectBoards = async (id) => {
   try {
-    const sql = `select * from boards`;
-    const [result] = await pool.query(sql, []);
+    const sql = `
+    select * 
+    from boards 
+    order by uid limit ?, 10
+    `;
+    const [result] = await pool.query(sql, [(id - 1) * 10]);
     return result;
   } catch (error) {
     throw new Error("[sql] selectBoards error: ", error.message);
+  }
+};
+
+exports.selectBoardsWhereKeyword = async (keyword, id) => {
+  try {
+    const sql = `
+    select * 
+    from boards 
+    where title like ? 
+    order by uid limit ?, 10
+    `;
+    const [[result]] = await pool.query(sql, [`%${keyword}%`, (id - 1) * 10]);
+    return result;
+  } catch (error) {
+    throw new Error("[sql] selectBoardsWhereKeyword error: ", error.message);
   }
 };
